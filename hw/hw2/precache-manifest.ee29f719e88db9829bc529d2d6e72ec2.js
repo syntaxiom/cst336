a4e558e4513bc1a73d4794df4878d434dae3a706
Sean <slester-wilson@csumb.edu>
2019-02-26 08:52:01 +0000

    Finished hw2

---
+++ hw/hw2/precache-manifest.ee29f719e88db9829bc529d2d6e72ec2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "fc077f7311c80e0cf767",
+    "url": "/static/js/main.fc077f73.chunk.js"
+  },
+  {
+    "revision": "8e33b7ff77b468a54e52",
+    "url": "/static/js/2.8e33b7ff.chunk.js"
+  },
+  {
+    "revision": "fc077f7311c80e0cf767",
+    "url": "/static/css/main.ef442097.chunk.css"
+  },
+  {
+    "revision": "7b49b8eba534d7062b57d664dc9e266e",
+    "url": "/index.html"
+  }
+];
