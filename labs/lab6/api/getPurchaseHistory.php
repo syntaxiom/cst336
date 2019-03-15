f3b8f6c930e974cbd466b64a0069e53ccd1bae20
Sean <slester-wilson@csumb.edu>
2019-03-15 23:05:02 +0000

    Finished Lab 6

---
+++ labs/lab6/api/getPurchaseHistory.php
@@ -0,0 +1,21 @@
+<?php
+
+    include '../../../inc/dbConnection.php';
+    
+    $conn = getDatabaseConnection("ottermart");
+    
+    $productId = $_GET['productId'];
+    $sql = "SELECT *
+            FROM om_product
+            NATURAL JOIN om_purchase
+            WHERE productId = :pId";
+            
+    $np = array();
+    $np[':pId'] = $productId;
+    
+    $stmt = $conn -> prepare($sql);  //$connection MUST be previously initialized
+    $stmt->execute($np);
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC); //use fetch for one record, fetchAll for multiple
+    echo json_encode($records);
+
+?>
