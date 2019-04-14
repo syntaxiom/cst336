7196238a5cd7fa2790814afd005513d967e7c948
Sean <slester-wilson@csumb.edu>
2019-04-14 04:34:25 +0000

    Finished Lab7 (for Heroku, v11).

--- labs/lab7/api/getProducts.php
+++ labs/lab7/api/getProducts.php
@@ -2,10 +2,10 @@
 
 // header('Access-Control-Allow-Origin: *');
 include '../../../inc/dbConnection.php';
-$conn = getDatabaseConnection("ottermart");
+$conn = getDatabaseConnection("heroku_866c6a4a6a8b135");
 
 $sql = "SELECT * FROM om_product ORDER BY price";
-$stmt = $dbConn -> prepare($sql);  //$connection MUST be previously initialized
+$stmt = $conn -> prepare($sql);  //$connection MUST be previously initialized
 $stmt->execute();
 $records = $stmt->fetchAll(PDO::FETCH_ASSOC); //use fetch for one record, fetchAll for multiple
 
