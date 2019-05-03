afafa49452f5194d29b6b4133ab5fe64de583e16
Sean <slester-wilson@csumb.edu>
2019-05-03 21:37:02 +0000

    Can I get literally anything to work? 4

--- final/api/_db_connection.php
+++ final/api/_db_connection.php
@@ -1,11 +1,11 @@
 <?php
-
+header("Access-Control-Allow-Origin: *");
     function get_database_connection($dbname) {
     
         $host = "us-cdbr-iron-east-03.cleardb.net";
         $username = "bf9749165752e5";
         $password = "d31bd484";
-        $dbname = "heroku_99567faf01a9180"; // should probably change this later
+        $dbname = "heroku_99567faf01a9180";
         
         //          user            pass        host                            something else
         // mysql://bf9749165752e5:d31bd484@us-cdbr-iron-east-03.cleardb.net/heroku_99567faf01a9180?reconnect=true
@@ -14,16 +14,15 @@
         
         
         //checks whether the URL contains "herokuapp" (using Heroku)
-        if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-           $url = parse_url(getenv("FINAL_DATABASE_URL")); //
-           $host = $url["host"];
-           $dbname = substr($url["path"], 1);
-           $username = $url["user"];
-           $password = $url["pass"];
-        }
+        // if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        //   $url = parse_url(getenv("FINAL_DATABASE_URL")); //
+        //   $host = $url["host"];
+        //   $dbname = substr($url["path"], 1);
+        //   $username = $url["user"];
+        //   $password = $url["pass"];
+        // }
         
         $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-        $dbConn -> exec("SET CHARACTER SET utf8");
         $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
         
         return $dbConn;
