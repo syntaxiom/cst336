75d683a7e81f9aed005f9a7bd92ba73dcf5d5ebe
Sean <slester-wilson@csumb.edu>
2019-05-03 21:45:07 +0000

    Can I get literally anything to work? 5

--- final/api/_db_connection.php
+++ final/api/_db_connection.php
@@ -14,13 +14,13 @@ header("Access-Control-Allow-Origin: *");
         
         
         //checks whether the URL contains "herokuapp" (using Heroku)
-        // if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-        //   $url = parse_url(getenv("FINAL_DATABASE_URL")); //
-        //   $host = $url["host"];
-        //   $dbname = substr($url["path"], 1);
-        //   $username = $url["user"];
-        //   $password = $url["pass"];
-        // }
+        if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+          $url = parse_url(getenv("CLEARDB_DATABASE_URL")); //
+          $host = $url["host"];
+          $dbname = substr($url["path"], 1);
+          $username = $url["user"];
+          $password = $url["pass"];
+        }
         
         $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
