ce24bc6cf5ca30e63b1364120795627cb546d208
Sean <slester-wilson@csumb.edu>
2019-05-03 07:23:02 +0000

    Can I get literally anything to work?

--- final/api/categories.php
+++ final/api/categories.php
@@ -1,15 +1,17 @@
 <?php
     header("Access-Control-Allow-Origin: *");
     
-    if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
-        return;
-    }
+    // if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    //     return;
+    // }
     
-    include './dbConnection.php';
-    $conn = getDatabaseConnection("store");
-    $sql = "SELECT catId, catName, catDesc FROM categories ORDER BY catName";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute();
-    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($records);
+    $arr = array();
+    $arr['hello'] = 'world';
+    // include './dbConnection.php';
+    // $conn = getDatabaseConnection("store");
+    // $sql = "SELECT catId, catName, catDesc FROM categories ORDER BY catName";
+    // $stmt = $conn->prepare($sql);
+    // $stmt->execute();
+    // $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($arr);
 ?>
