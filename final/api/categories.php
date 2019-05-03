4869ba31c165cda87ee4d0e7baf5905055440193
Sean <slester-wilson@csumb.edu>
2019-05-03 04:53:46 +0000

    Experimenting with db_connection path 2

--- final/api/categories.php
+++ final/api/categories.php
@@ -5,7 +5,7 @@
         return;
     }
     
-    include '../inc/dbConnection.php';
+    include '../../inc/dbConnection.php';
     $conn = get_database_connection("store");
     $sql = "SELECT catId, catName, catDesc FROM categories ORDER BY catName";
     $stmt = $conn->prepare($sql);
