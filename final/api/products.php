4e9bbf0531b73d05380e2bc9596547300b13859d
Sean <slester-wilson@csumb.edu>
2019-05-08 18:51:55 +0000

    Bug fixes products.php 8

--- final/api/products.php
+++ final/api/products.php
@@ -1,4 +1,6 @@
 <?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PATCH, DELETE");
     session_start();
     
     include '_db_connection.php';
