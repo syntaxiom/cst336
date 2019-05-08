69b358a89837b4bc9d73133ee99e4e9211844b7d
Sean <slester-wilson@csumb.edu>
2019-05-08 21:02:53 +0000

    Will session work? 2

--- final/api/session.php
+++ final/api/session.php
@@ -1,32 +1,14 @@
 <?php
     header("Access-Control-Allow-Origin: *");
-    
+    header("Access-Control-Allow-Methods: GET, POST, DELETE");
     session_start();
     
     include '_db_connection.php';
-    $conn = get_database_connection("store");
 
-
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-        $binder = array(
-            "username" => $_POST["username"],
-            "password" => $_POST["password"],
-        );
     
-        $sql = "SELECT username FROM shadow_admin WHERE username = :username AND `password` = SHA1(:password);";
+    $binder = json_decode(file_get_contents("php://input"), TRUE);
     
-        $stmt = $conn->prepare($sql);
-        $stmt->execute($binder);
-        $records = $stmt->fetch(PDO::FETCH_ASSOC);
-        
-        if (isset($records['username'])){
-            $_SESSION["username"] = $records["username"];
-        }
-    
-        echo json_encode($records);
-        return;
-    } else if ($_SERVER['REQUEST_METHOD'] === 'GET'){
+    if ($_SERVER['REQUEST_METHOD'] === 'GET'){
 
         if (!isset($_SESSION['username'])) {
     
@@ -42,7 +24,34 @@
         echo json_encode($arr);
         return;
         
-    } else if ($_SERVER['REQUEST_METHOD'] === 'DELETE'){
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        if (isset($binder["action"])){
+            $_SERVER['REQUEST_METHOD'] = strtoupper($binder["action"]);
+            unset($binder["action"]);
+        } else {
+            
+            $conn = get_database_connection("store");
+            
+            $sql = "SELECT username FROM shadow_admin WHERE username = :username AND `password` = SHA1(:password);";
+            
+            $stmt = $conn->prepare($sql);
+            $stmt->execute($binder);
+            $records = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            if (isset($records['username'])){
+                $_SESSION["username"] = $records["username"];
+            }
+        
+            echo json_encode($records);
+            return;
+        }
+        
+    }
+    
+    if ($_SERVER['REQUEST_METHOD'] === 'DELETE'){
         
         session_destroy();
         echo "true";
